78fd1b61b6de828df48456300b4d11fa35ffe797
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-06-29 13:36:34 +0900

    feat 사이즈 조절 및 슬롯

--- Assets/02.Scripts/Board/CardSlot.cs
+++ Assets/02.Scripts/Board/CardSlot.cs
@@ -2,5 +2,11 @@ using Photon.Pun;
 using UnityEngine;
 public class CardSlot : MonoBehaviourPunCallbacks
 {
-    
+    private Card _card;
+    public Card Card => _card;
+
+    public void Refresh(Card card)
+    {
+        _card = card;
+    }
 }
