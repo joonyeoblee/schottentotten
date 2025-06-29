c1941b223f83e6a08ad41febd7134a4e74a2af2c
Giyeon Na <giyeonna@gmail.com>
2025-06-30 00:08:25 +0900

    Feat 플레이어간 카드검증 로직 테스트툴 추가

--- Assets/02.Scripts/Card/1.Domain/Card.cs
+++ Assets/02.Scripts/Card/1.Domain/Card.cs
@@ -14,5 +14,16 @@ public class Card
 {
     public int CardNumber;
     public ECardColor Color;
-    
+
+    public Card(int cardNumber, ECardColor color)
+    {
+        CardNumber = cardNumber;
+        Color = color;
+    }
+
+    public Card()
+    {
+        CardNumber = 1;
+        Color = ECardColor.Red; 
+    }
 }
